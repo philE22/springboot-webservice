33e1a9ec1e1768b59834c3c4038cb886081087cb
philE22 <moonwhlking3@naver.com>
2022-05-05 20:37:06 +0900

    ~p.124 3장 스프링부트에서 JPA로 데이터베이스 다뤄보자 끝.

--- src/main/java/com/phile/book/springboot/Application.java
+++ src/main/java/com/phile/book/springboot/Application.java
@@ -2,7 +2,9 @@ package com.phile.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing 활성화 기능
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
