a78074a7aff6f599df418cf08b24047391536f5c
philE22 <moonwhlking3@naver.com>
2022-05-06 22:47:09 +0900

    ~p.161 4장 머스테치로 화면 구성하기 끝

--- src/main/java/com/phile/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/phile/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.phile.book.springboot.service.posts;
 
 import com.phile.book.springboot.domain.post.Posts;
 import com.phile.book.springboot.domain.post.PostsRepository;
+import com.phile.book.springboot.web.dto.PostsListResponseDto;
 import com.phile.book.springboot.web.dto.PostsResponseDto;
 import com.phile.book.springboot.web.dto.PostsSaveRequestDto;
 import com.phile.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,6 +33,13 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
@@ -37,7 +47,11 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
-
-
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
 }
