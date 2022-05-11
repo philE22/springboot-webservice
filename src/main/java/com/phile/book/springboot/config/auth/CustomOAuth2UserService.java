20392d71331480eacde8d14306d00b927184540b
philE22 <moonwhlking3@naver.com>
2022-05-12 01:12:09 +0900

    ~p.194 기본적인 구글 로그인, 로그아웃, 회원가입, 권한관리 기능 구현

--- src/main/java/com/phile/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/phile/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -1,5 +1,7 @@
 package com.phile.book.springboot.config.auth;
 
+import com.phile.book.springboot.config.auth.dto.OAuthAttributes;
+import com.phile.book.springboot.config.auth.dto.SessionUser;
 import com.phile.book.springboot.domain.user.User;
 import com.phile.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -36,14 +38,15 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())), attributes.getAttributes(), attributes.getNameAttributeKey());
+    }
 
-        private User saveOrUpdate (OAuthAttributes attributes){
-            userRepository.findByEmail(attributes.getEmail())
-                    .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
-                    .orElse(attributes.toEntity());
-
-        }
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
 
         return userRepository.save(user);
     }
+
+
 }
