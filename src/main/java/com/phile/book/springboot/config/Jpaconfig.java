482d1ea4ef67a388bf1c6e527100363e0eaf97bc
philE22 <moonwhlking3@naver.com>
2022-05-13 22:18:39 +0900

    ~p.224 챕터5 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기 끝

---
+++ src/main/java/com/phile/book/springboot/config/Jpaconfig.java
@@ -0,0 +1,9 @@
+package com.phile.book.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing //JPA Auditing 활성화 기능
+public class Jpaconfig {
+}
--- src/main/java/com/phile/book/springboot/web/IndexController.java
+++ src/main/java/com/phile/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.phile.book.springboot.web;
 
+import com.phile.book.springboot.config.auth.LoginUser;
 import com.phile.book.springboot.config.auth.dto.SessionUser;
 import com.phile.book.springboot.service.posts.PostsService;
 import com.phile.book.springboot.web.dto.PostsResponseDto;
@@ -19,10 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
